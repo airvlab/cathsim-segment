8b43db1d3d3cd67a8f50633d035f3dcbc2ace79c
Tudor Jianu <tudorjnu@gmail.com>
2024-07-18 15:45:44 +0100

    add transforms

--- src/cathseg/mobile_unet/train.py
+++ src/cathseg/mobile_unet/train.py
@@ -4,6 +4,17 @@ import pytorch_lightning as pl
 import torch.utils.data as data
 from cathseg.mobile_unet.network import MobileUNetLightning
 from guide3d.dataset.segment import Guide3D
+from torchvision import transforms
+
+image_transforms = transforms.Compose(
+    [
+        # transforms.Resize((256, 256)),
+        transforms.Lambda(lambda x: x / 255.0),
+        transforms.Normalize((0.5,), (0.5,)),
+        # gray to RGB
+        # transforms.Lambda(lambda x: x.repeat(3, 1, 1))
+    ]
+)
 
 
 def main():
